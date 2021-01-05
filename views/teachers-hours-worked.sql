485a32b3edc0da0f0b10cb985467ca943598200d
pagmerek <p.gmerek@student.uj.edu.pl>
2021-01-05 19:02:27 +0100

    Add hours-worked view

---
+++ views/teachers-hours-worked.sql
@@ -0,0 +1,8 @@
+--widok pokazuje ilość przepracowanych godzin lekcyjnych
+
+CREATE VIEW hours_worked_per_teacher AS
+    SELECT T.FirstName Imie, T.LastName Nazwisko, SUM(P.hours) [Przepracowane godziny] 
+    FROM Teachers T 
+    JOIN Courses C ON T.TeacherId = C.TeacherId
+    JOIN (SELECT CourseId, COUNT(*) hours FROM Lessons) P ON P.CourseId = C.CourseId
+    GROUP BY Imie, Nazwisko
