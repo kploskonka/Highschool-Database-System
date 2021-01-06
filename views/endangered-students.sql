866a0801eeae2f453e9616d4ae005fc1d9295661
pagmerek <p.gmerek@student.uj.edu.pl>
2021-01-06 14:31:28 +0100

    add almost all insert_data (except for Borrows) and some minor table fix

--- views/endangered-students.sql
+++ views/endangered-students.sql
@@ -2,8 +2,10 @@
 --jako zagrożenie uznaje średnią poniżej 2.0
 
 CREATE VIEW endangered_students AS
-    SELECT S.Name Imię, S.Surname Nazwisko,CourseName Przedmiot, ROUND(AVG(Mark),2) srednia
+    SELECT S.FirstName [Imię], S.LastName [Nazwisko],C.CourseName [Przedmiot], ROUND(AVG(Mark),2) [srednia]
     FROM ExamResults ER
     JOIN Students S ON S.StudentId = ER.StudentId
-    GROUP BY Imię, Nazwisko, CourseName
+    JOIN Exams E ON ER.ExamId = E.ExamId
+    JOIN Courses C ON E.CourseId = C.CourseId
+    GROUP BY S.FirstName, S.LastName, C.CourseName
     HAVING ROUND(AVG(Mark),2) < 2.0
--- views/teachers-hours-worked.sql
+++ views/teachers-hours-worked.sql
@@ -4,5 +4,6 @@ CREATE VIEW hours_worked_per_teacher AS
     SELECT T.FirstName Imie, T.LastName Nazwisko, SUM(P.hours) [Przepracowane godziny] 
     FROM Teachers T 
     JOIN Courses C ON T.TeacherId = C.TeacherId
-    JOIN (SELECT CourseId, COUNT(*) hours FROM Lessons) P ON P.CourseId = C.CourseId
-    GROUP BY Imie, Nazwisko
+    JOIN (SELECT CourseId, COUNT(*) hours FROM Lessons GROUP BY CourseId) P ON P.CourseId = C.CourseId
+    GROUP BY T.FirstName, T.LastName
+    
