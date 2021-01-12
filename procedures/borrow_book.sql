e96b6e3147b1c303f5e912458d7894782f81e53d
pagmerek <p.gmerek@student.uj.edu.pl>
2021-01-12 21:53:16 +0100

    borrow book procedure and F for notes trigger

---
+++ procedures/borrow_book.sql
@@ -0,0 +1,19 @@
+CREATE PROCEDURE borrow_book @AccountLogin nvarchar(30), @Password nvarchar(30), @Title nvarchar(30)
+AS
+    IF EXISTS(SELECT AccountId FROM Accounts WHERE Login = @AccountLogin AND Password = @Password) BEGIN
+            BEGIN TRANSACTION
+                IF (SELECT Amount FROM Books WHERE Title = @Title) > 0 BEGIN
+                    INSERT INTO Borrows VALUES
+                        ((SELECT StudentId FROM Students S JOIN Accounts A ON S.AccountId = A.AccountId WHERE A.Login = @AccountLogin),
+                        (SELECT BookId FROM Books WHERE Title = @Title),
+                        GETDATE()
+                        )
+                    UPDATE Books
+                        SET Amount = (SELECT Amount FROM Books WHERE Title = @Title) - 1
+                        WHERE Title = @Title
+                    END
+                ELSE PRINT "Nie wystarczająca liczba książek w bibliotece o podanym tytule";
+    END
+    ELSE BEGIN
+            PRINT "Nie ma użytkownika o takim haśle/loginie"
+        END
---
+++ triggers/tr_grade_for_notes.sql
@@ -0,0 +1,10 @@
+CREATE TRIGGER tr_grade_for_notes
+ON Notes
+AFTER INSERT
+AS
+    IF(SELECT count(*) FROM Notes WHERE StudentId = (SELECT StudentId FROM inserted)) >= 3
+        BEGIN
+            INSERT INTO Grades VALUES
+                ((SELECT StudentId FROM inserted),(SELECT TeacherId FROM inserted),1,'Trzy uwagi')
+        END
+GO
