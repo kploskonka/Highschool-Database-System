e7e7c7e222a7e00d66023d3b10cdc6f5a9feff7d
pagmerek <p.gmerek@student.uj.edu.pl>
2021-01-31 15:14:17 +0100

    trigger fix

--- triggers/tr_grade_for_activity.sql
+++ triggers/tr_grade_for_activity.sql
@@ -1,9 +1,9 @@
 CREATE TRIGGER grade_for_activity ON Grades
     AFTER INSERT
-        if(inserted.Description = '+' AND (SELECT count(*) FROM Grades WHERE StudentId = inserted.StudentId 
+        IF((SELECT Description FROM inserted) = '+' AND (SELECT count(*) FROM Grades WHERE StudentId = (SELECT StudentId FROM inserted)
                                             AND Description = '+')%5 = 0)
         BEGIN
-            INSERT INTO Grades(inserted.TeacherId, inserted.StudentId, 5, 'Aktywność')
+            INSERT INTO Grades((SELECT TeacherId from inserted), (SELECT StudentId from inserted), 5, 'Aktywność')
         END
 
     END
--- triggers/tr_grade_for_exam.sql
+++ triggers/tr_grade_for_exam.sql
@@ -1,10 +1,10 @@
 CREATE TRIGGER grade_from_exam ON ExamResults
 		FOR INSERT 
 		AS 
-			INSERT INTO Grades VALUES (inserted.StudentId, (
+			INSERT INTO Grades VALUES ((SELECT StudentId FROM inserted), (
 									SELECT T.TeacherId FROM Teachers 
 									JOIN Coruses C ON C.TeacherId = T.TeacherId
 									JOIN Exams E ON E.CourseId = C.CourseId
-									JOIN ExamResults ER ON ER.ExamId = inserted.ExamId)
+									JOIN ExamResults ER ON ER.ExamId = (SELECT ExamId FROM inserted))
 									)
 			END
