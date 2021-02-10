92e3f5506617a7a5252386d19071f3b27b4890d9
Katarzyna Płoskonka <kasplosk@gmail.com>
2021-02-10 16:24:23 +0100

    Fix typo

--- triggers/tr_grade_for_exam.sql
+++ triggers/tr_grade_for_exam.sql
@@ -3,7 +3,7 @@ CREATE TRIGGER grade_from_exam ON ExamResults
 		AS 
 			INSERT INTO Grades VALUES ((SELECT StudentId FROM inserted), (
 									SELECT T.TeacherId FROM Teachers 
-									JOIN Coruses C ON C.TeacherId = T.TeacherId
+									JOIN Courses C ON C.TeacherId = T.TeacherId
 									JOIN Exams E ON E.CourseId = C.CourseId
 									JOIN ExamResults ER ON ER.ExamId = (SELECT ExamId FROM inserted)),
 									(SELECT Mark FROM inserted),
